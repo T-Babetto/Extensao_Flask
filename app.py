84ed4524ce2ae4ed4f763b38377aa3bac361ffbf
Tiago Rocha <tiagorocha.una@gmail.com>
2022-09-28 13:13:01 -0300

    Change app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+from http import client
 from flask import Flask, Response, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 import json
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clientes.sqlite3'
 
 db = SQLAlchemy(app)
 
+
 class Cliente(db.Model):
-    id = db.Column('id',db.Integer, primary_key= True, autoincrement=True)
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(50))
     email = db.Column(db.String(100))
 
@@ -17,17 +19,42 @@ class Cliente(db.Model):
         self.nome = nome
         self.email = email
 
+
 @app.route('/')
 def index():
-    cliente = Cliente.query.all()
-    return render_template('index.html', cliente=cliente)
+    clientes = Cliente.query.all()
+    return render_template('index.html', clientes=clientes)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
+        cliente = Cliente(request.form['nome'], request.form['email'])
+        db.session.add(cliente)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('add.html')    
+    return render_template('add.html')
+
+
+@app.route('/edit/<int:id>', methods=['GET, POST'])
+def edit(id):
+    cliente = Cliente.query.get(id)
+    if request.method == 'POST':
+        cliente.nome = request.form['nome']
+        cliente.email = request.form['email']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', cliente=cliente)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    cliente = Cliente.query.get(id)
+    db.session.delete(cliente)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)   
+    app.run(debug=True)
